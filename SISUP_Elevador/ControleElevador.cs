929c8732b805666aa9f78c6ad557695aee8ff847
bielusantos <bielusantos@ucl.br>
2021-11-16 19:04:41 -0300

    adição do modulo de controle e renomeando a namespace

---
+++ SISUP_Elevador/ControleElevador.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace SISUP_Elevador
+{
+    public class ControleElevador
+    {
+        public Estado estadoAtual;
+        private int andares;
+
+        public ControleElevador(int andaresPredio)
+        {
+            andares = andaresPredio;
+            estadoAtual = new ElevadorParado(1, this);
+
+        }
+
+        public void IniciarElevador()
+        {
+            while (true)
+            {
+                Console.WriteLine(estadoAtual.MovimentarElevador());
+            }
+        }
+        
+        public int obterAndares()
+        {
+            return andares;
+        }
+
+        public int validateEntrada()
+        {
+            newEntrada:
+            var entrada = Console.ReadLine();
+
+            if (entrada == "0")
+                Environment.Exit(0);
+
+            if (!int.TryParse(entrada, out int resultado))
+            {
+                Console.WriteLine("Configurar entrada do botao para valor somente numerico ou colocar um parse");
+                goto newEntrada;
+            }
+            
+            return resultado;
+        }
+
+    }
+}
